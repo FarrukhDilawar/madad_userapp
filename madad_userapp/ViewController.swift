cbe5b5ce7b698b78eaf3b16abe44ebef3346a3ff
Fauji007 <farrukhdilawar198@gmail.com>
2018-01-17 10:47:53 +0500

    Initial Commit

---
+++ madad_userapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  madad_userapp
+//
+//  Created by Farrukh on 17/01/2018.
+//  Copyright © 2018 assassindeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
